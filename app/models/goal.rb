475f559d1b2c2e31db8ae135a52c4e9722574a0d
Raghu Betina <raghu@firstdraft.com>
2019-03-11 19:34:13 +0000

    Direct association between goals and customized_preferences

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,9 @@
 class Goal < ApplicationRecord
   # Direct associations
 
+  has_many   :customized_preferences,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Goal, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:customized_preferences) }
+
     it { should belong_to(:user) }
 
     end
