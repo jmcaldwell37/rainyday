60fdce11c5b80f568e3fbada3dee91b809b1278c
Raghu Betina <raghu@firstdraft.com>
2019-03-11 19:33:55 +0000

    Generate customized_preferences resource with draft:resource

---
+++ app/models/customized_preference.rb
@@ -0,0 +1,8 @@
+class CustomizedPreference < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Customized preference resource:
+
+  # CREATE
+  get("/customized_preferences/new", { :controller => "customized_preferences", :action => "new_form" })
+  post("/create_customized_preference", { :controller => "customized_preferences", :action => "create_row" })
+
+  # READ
+  get("/customized_preferences", { :controller => "customized_preferences", :action => "index" })
+  get("/customized_preferences/:id_to_display", { :controller => "customized_preferences", :action => "show" })
+
+  # UPDATE
+  get("/customized_preferences/:prefill_with_id/edit", { :controller => "customized_preferences", :action => "edit_form" })
+  post("/update_customized_preference/:id_to_modify", { :controller => "customized_preferences", :action => "update_row" })
+
+  # DELETE
+  get("/delete_customized_preference/:id_to_remove", { :controller => "customized_preferences", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Goal resource:
 
   # CREATE
