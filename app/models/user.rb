da914015086149db67bf9abad9494130011eff91
Raghu Betina <raghu@firstdraft.com>
2019-03-11 19:34:13 +0000

    Direct association between users and memberships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :memberships,
+             :dependent => :destroy
+
   has_many   :customized_preferences,
              :dependent => :destroy
 
